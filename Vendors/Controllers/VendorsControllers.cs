038174999c2dec7a6ee37349785de0b3eaa091dc
Alejae1998 <alejagr1998@gmail.com>
2023-07-21 16:20:31 -0700

    controllers

--- Vendors/Controllers/VendorsControllers.cs
+++ Vendors/Controllers/VendorsControllers.cs
@@ -3,4 +3,39 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using Vendors.Models;
 
-namespace
+namespace Vendors.Controllers
+{
+    public class VendorsControllers : Controller
+    {
+        [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(name, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+    }
+}
