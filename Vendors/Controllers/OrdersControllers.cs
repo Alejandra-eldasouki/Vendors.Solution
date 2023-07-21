28418e0e9138377a4aa285b5d06ccefe2452626d
Alejae1998 <alejagr1998@gmail.com>
2023-07-21 16:01:12 -0700

    CONTROLLERS

---
+++ Vendors/Controllers/OrdersControllers.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Vendors.Models;
+using System.Collections.Generic;
+
+namespace Vendors.Controllers
+{
+    public class OrdersController : Controller
+    {
+        
+    }
+}
