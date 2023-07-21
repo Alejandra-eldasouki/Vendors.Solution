6895b0ca1da9930873486449300b6e1885b7783a
Alejae1998 <alejagr1998@gmail.com>
2023-07-21 15:52:05 -0700

    working on controllers

---
+++ Vendors/Controllers/Homecontrollers.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vendors.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
