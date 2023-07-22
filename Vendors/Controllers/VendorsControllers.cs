cf646f417bff86ca239ecc0f9848edfa05e344d8
Alejae1998 <alejagr1998@gmail.com>
2023-07-22 00:32:12 -0700

    fixed path issue

--- Vendors/Controllers/VendorsControllers.cs
+++ Vendors/Controllers/VendorsControllers.cs
@@ -5,37 +5,37 @@ using Vendors.Models;
 
 namespace Vendors.Controllers
 {
-    public class VendorsControllers : Controller
+    public class VendorsController : Controller
     {
         [HttpGet("/vendors")]
-    public ActionResult Index()
-    {
-      List<Vendor> allVendors = Vendor.GetAll();
-      return View(allVendors);
-    }
+        public ActionResult Index()
+        {
+            List<Vendor> allVendors = Vendor.GetAll();
+            return View(allVendors);
+        }
 
-    [HttpGet("/vendors/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+        [HttpGet("/vendors/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
 
-    [HttpPost("/vendors")]
-    public ActionResult Create(string name, string vendorDescription)
-    {
-      Vendor newVendor = new Vendor(name, vendorDescription);
-      return RedirectToAction("Index");
-    }
+        [HttpPost("/vendors")]
+        public ActionResult Create(string name, string vendorDescription)
+        {
+            Vendor newVendor = new Vendor(name, vendorDescription);
+            return RedirectToAction("Index");
+        }
 
-    [HttpGet("/vendors/{id}")]
-    public ActionResult Show(int id)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor selectedVendor = Vendor.Find(id);
-      List<Order> vendorOrders = selectedVendor.Orders;
-      model.Add("vendor", selectedVendor);
-      model.Add("orders", vendorOrders);
-      return View(model);
-    }
+        [HttpGet("/vendors/{id}")]
+        public ActionResult Show(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor selectedVendor = Vendor.Find(id);
+            List<Order> vendorOrders = selectedVendor.Orders;
+            model.Add("vendor", selectedVendor);
+            model.Add("orders", vendorOrders);
+            return View(model);
+        }
     }
 }
