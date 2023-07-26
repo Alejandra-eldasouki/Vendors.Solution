9bdc5da5d4b00d747696c072dd4b1841cd37e614
Alejae1998 <alejagr1998@gmail.com>
2023-07-26 15:02:48 -0700

    trying to fix

--- Vendors/Controllers/OrdersControllers.cs
+++ Vendors/Controllers/OrdersControllers.cs
@@ -6,23 +6,46 @@ namespace Vendors.Controllers
 {
     public class OrdersController : Controller
     {
+        // Other actions in the OrdersController...
+
         [HttpGet("/vendors/{vendorId}/orders/new")]
-    public ActionResult New(int vendorId)
+        public ActionResult New(int vendorId)
+        {
+            Vendor vendor = Vendor.Find(vendorId);
+            return View(vendor);
+        }
+
+        [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+        public ActionResult Show(int vendorId, int orderId)
+        {
+            Order order = Order.Find(orderId);
+            Vendor vendor = Vendor.Find(vendorId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            model.Add("order", order);
+            model.Add("vendor", vendor);
+            return View(model);
+        }
+
+        [HttpPost("/vendors/{vendorId}/orders")]
+       [HttpPost("/vendors/{vendorId}/orders")]
+public ActionResult CreateOrder(int vendorId, string orderTitle, string description, double price, string date)
+{
+    Vendor vendor = Vendor.Find(vendorId);
+    if (vendor == null)
     {
-      Vendor vendor = Vendor.Find(vendorId);
-      return View(vendor);
+        return NotFound();
     }
 
+    // Convert the 'price' parameter to an int if needed (assuming the Order model uses int for Price)
+    int convertedPrice = (int)price;
 
-    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
-    public ActionResult Show(int vendorId, int orderId)
-    {
-      Order order = Order.Find(orderId);
-      Vendor vendor = Vendor.Find(vendorId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("order", order);
-      model.Add("vendor", vendor);
-      return View(model);
-    }
+    Order newOrder = new Order(orderTitle, description, convertedPrice, date);
+
+    // Add the new order to the vendor's list of orders
+    vendor.AddOrder(newOrder);
+
+    // Redirect to the vendor's details page after creating the order
+    return RedirectToAction("Show", "Vendors", new { id = vendorId });
+}
     }
 }
